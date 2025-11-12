32d9fe835427fee1dcb8edf71a0828a01a8afa04
Walter Neto <contato.walter@proton.me>
2025-11-12 15:37:51 -0300

    feat: create Empresa model

---
+++ src/myfood/models/Empresa.java
@@ -0,0 +1,60 @@
+package myfood.models;
+
+public class Empresa {
+    private int id;
+    private String nome;
+    private String endereco;
+    private String tipoCozinha;
+    private DonoEmpresa dono;
+
+    public Empresa() {
+    }
+
+    public Empresa(int id, String nome, String endereco, String tipoCozinha, DonoEmpresa dono) {
+        this.id = id;
+        this.nome = nome;
+        this.endereco = endereco;
+        this.tipoCozinha = tipoCozinha;
+        this.dono = dono;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public String getTipoCozinha() {
+        return tipoCozinha;
+    }
+
+    public DonoEmpresa getDono() {
+        return dono;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public void setTipoCozinha(String tipoCozinha) {
+        this.tipoCozinha = tipoCozinha;
+    }
+
+    public void setDono(DonoEmpresa dono) {
+        this.dono = dono;
+    }
+}
