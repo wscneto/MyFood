92284ef98b83b8aed7fe8537a94c38c26d199968
Walter Neto <contato.walter@proton.me>
2025-11-12 15:40:14 -0300

    pass us2

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,8 @@ public class Main {
 
         EasyAccept.main(new String[] { facade, "tests/us1_1.txt" });
         EasyAccept.main(new String[] { facade, "tests/us1_2.txt" });
-        // EasyAccept.main(new String[] { facade, "tests/us2_1.txt" });
-        // EasyAccept.main(new String[] { facade, "tests/us2_2.txt" });
+        EasyAccept.main(new String[] { facade, "tests/us2_1.txt" });
+        EasyAccept.main(new String[] { facade, "tests/us2_2.txt" });
         // EasyAccept.main(new String[] { facade, "tests/us3_1.txt" });
         // EasyAccept.main(new String[] { facade, "tests/us3_2.txt" });
         // EasyAccept.main(new String[] { facade, "tests/us4_1.txt" });
